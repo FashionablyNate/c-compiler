cf71fcdf5183262536b56b0f6de57693cb2bdcd9
Nathan Spelts <nate@nathanspelts.com>
2024-02-01 20:42:42 -0800

    Wrote a program that reads from stdin and writes it to stdout.

--- src/HelloWorld.s
+++ src/HelloWorld.s
@@ -1,26 +1,28 @@
-// Assembler program to print "Hello World!"
-// to stdout.
-//
-// X0-X2 - parameters to linux function services
-// X16 - linux function number
-//
-.global _start             // Provide program starting address to linker
-.p2align 3 // Feedback from Peter
+.global _start 
+.p2align 2
 
-// Setup the parameters to print hello world
-// and then call Linux to do it.
+_start: 
+        // manage the stack
+        sub sp, sp, #64                 // Subtract 64 from the stack pointer to allocate stack space
+        stp	x29, x30, [sp, #48]     // Save the frame pointer and return address on the stack
+        add	x29, sp, #48            // Set up the new frame pointer
+        mov	w0, #0                  // Set the return value of the function to 0
+	str	wzr, [sp, #24]          // Clear a space on the stack (zero register to stack)
 
-_start: mov X0, #1     // 1 = StdOut
-        adr X1, helloworld // string to print
-        mov X2, #13     // length of our string
-        mov X16, #4     // MacOS write system call
-        svc 0     // Call linux to output the string
+        // read from stdin
+	sub	x1, x29, #18    // Calculate the buffer address
+	str	x1, [sp, #8]    // Store buffer address for read operation
+	mov	x2, #10         // Set the buffer size
+	str	x2, [sp, #16]   // Store buffer size for read operation
+	bl	_read           // Call read function
 
-// Setup the parameters to exit the program
-// and then call Linux to do it.
+        // write to stdout
+	ldr	x1, [sp, #8]    // Reload buffer address
+	ldr	x2, [sp, #16]   // Reload buffer size
+	mov	w0, #1          // Set file descriptor to STDOUT
+	bl	_write          // Call write function
 
-        mov     X0, #0      // Use 0 return code
-        mov     X16, #1     // Service command code 1 terminates this program
-        svc     0           // Call MacOS to terminate the program
-
-helloworld:      .ascii  "Hello World!\n"
+        // return
+	mov     X0, #0          // Use 0 return code
+        mov     X16, #1         // Service command code 1 terminates this program
+        svc     0               // Call MacOS to terminate the program
